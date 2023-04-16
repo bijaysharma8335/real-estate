c0fa9d250f69ef14aab6e195f729d8586616df51
Bijay Sharma <bijaysharma8335@gmail.com>
2023-04-16 15:32:50 +0530

    Create header ,footer ,home page
    -Add all images in assets  folder and create some basic components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Pages/Home";
+import PropertyDetails from "./Pages/PropertyDetails";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Routes>
+                <Route path="/ " element={<Home />} />
+                <Route path="/property/:id" element={<PropertyDetails />} />
+            </Routes>
+            <Footer />
+        </div>
+    );
+};
 
 export default App;
---
+++ src/Pages/PropertyDetails.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PropertyDetails = () => {
+    return <div>PropertyDetail</div>;
+};
+
+export default PropertyDetails;
